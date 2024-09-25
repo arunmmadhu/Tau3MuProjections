e53251c756c6fc1901315721eab602b005017e52
Vladimir Cherepanov <vladimir.cherepanov@cern.ch>
2024-09-25 18:21:52 +0200

    add bins

--- makeYield/readLimits.py
+++ makeYield/readLimits.py
@@ -15,7 +15,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--category', required=True               , help='W, HF, ZTT',  dest='category'          , default='W') 
 parser.add_argument('-r', '--runType' , default='plot'              , help='Option run or plot', dest='runType'   )
-parser.add_argument('-m', '--Method' , default='A'              , help='Option A=Asymptotic, H=HybridNew', dest='Method' )
+parser.add_argument('-m', '--Method' , default='A'                  , help='Option A=Asymptotic, H=HybridNew', dest='Method' )
 args = parser.parse_args()
 
 
@@ -30,9 +30,14 @@ CMS_lumi.outOfFrame = True
 tdrstyle.setTDRStyle()
 
 
+
+
+lumi=[]
 lumi = [97.7,129.0,150,377.0,500.0,700.0,1000.0, 1200.0, 1500.0, 1700.0, 2000.0, 2250.0, 2500.0, 2750.0, 3000.0 ]
-#$lumi = [59.0,97.7, 129.0, 377.0, 700.0, 1500.0, 2250.0, 3000.0, 3750.0, 4500.0]
-#lumi = [59.0,97.7, 129.0]
+if(args.category=='W'):
+        lumi = [97.7,129.0,150,377.0,500.0,550.0, 600.0, 650.0, 700.0, 750.0, 800.0, 850.0, 900.0, 1000.0, 1050.0, 1100.0, 1200.0,
+                1250.0, 1300.0, 1350.0, 1400.0, 1450.0,  1500.0, 1550.0, 1600.0, 1650.0,1700.0, 2000.0, 2250.0, 2500.0, 2750.0, 3000.0]
+
 
 def executeDataCards_onCondor(lumi,categories,Whether_Hybrid):
         
