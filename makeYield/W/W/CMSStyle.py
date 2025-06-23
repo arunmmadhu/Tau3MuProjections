e6f9ee066df03290ea3a3c158d4882bf701bd08b
Arun M Madhu <arunmmadhu@gmail.com>
2025-06-23 04:30:04 +0200

    CMSStyle in python3

--- makeYield/W/W/CMSStyle.py
+++ makeYield/W/W/CMSStyle.py
@@ -9,7 +9,7 @@ cmsText     = "CMS";
 cmsTextFont   = 61  
 
 writeExtraText = True
-extraText   = "    work in progress"
+extraText   = "work in progress"
 extraTextFont = 52 
 
 lumiTextSize     = 0.5
@@ -32,7 +32,7 @@ extraOverCmsTextSize  = 0.8
 # lumi_13TeV = "56.8 pb^{-1}" # Run 2015B 3.8T
 # lumi_13TeV = "64.8 pb^{-1}" # Run 2015B total
 #lumi_13TeV = "7.0 pb^{-1}" # Run 2015A
-lumi_13TeV  = "58.8 fb^{-1}" 
+lumi_13TeV  = "59.8 fb^{-1}" 
 lumi_8TeV  = "19.7 fb^{-1}" 
 lumi_7TeV  = "5.1 fb^{-1}"
 
@@ -44,11 +44,11 @@ def CMS_lumi(pad,  iPeriod,  iPosX ):
 
     alignY_=3
     alignX_=2
-    if( iPosX/10==0 ): alignX_=1
+    if( iPosX//10==0 ): alignX_=1
     if( iPosX==0    ): alignY_=1
-    if( iPosX/10==1 ): alignX_=1
-    if( iPosX/10==2 ): alignX_=2
-    if( iPosX/10==3 ): alignX_=3
+    if( iPosX//10==1 ): alignX_=1
+    if( iPosX//10==2 ): alignX_=2
+    if( iPosX//10==3 ): alignX_=3
     align_ = 10*alignX_ + alignY_
 
     H = pad.GetWh()
